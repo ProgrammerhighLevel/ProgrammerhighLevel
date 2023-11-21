ac84caa0223d7e937bf8a59c33479de46a1b0ce9
ProgrammerhighLevel <151663242+ProgrammerhighLevel@users.noreply.github.com>
2023-11-21 18:00:34 +0100

    ggT Rechner

---
+++ ggT_calculator.py
@@ -0,0 +1,22 @@
+
+print("Hallo! Ich kann dir den ggT und den von zwei Zahlen berechnen")
+a = int(input("Zahl eins: "))
+b = int(input("Zahl zwei: "))
+a_ende = a
+b_ende = b
+f = 1
+Vertauscher = 0
+Rechnung = list()
+#Hier benutze ich den Eudlikischen Algorithmus 
+while f != 0 :
+  if a < b :
+    Vertauscher = a
+    a = b
+    b = Vertauscher
+  a_neu = a
+  a = a_neu % b
+  f = a
+  Rechnung.append(f)
+Ergebnis = Rechnung[(len(Rechnung)-2)]
+print("Der größte gemeinsame Nenner von " + str(a_ende) + " und " + str(b_ende) + " ist: ")
+print(str(Ergebnis))
